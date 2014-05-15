f9c0773ab56e11abec0435b48c01291c485c7c02
Robert Kemi <robert.kemi@gmail.com>
2014-05-15 16:35:45 +0200

    adding server

---
+++ server/sql/tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Phones (
+	id VARCHAR(30),
+	PRIMARY KEY (id) 
+);
+
+CREATE TABLE Aps (
+	bssid VARCHAR(17),
+	ssid VARCHAR(20),
+	latitude FLOAT,
+	longitude FLOAT,
+	PRIMARY KEY (bssid)
+);
+
+CREATE TABLE Logs (
+	time VARCHAR(30),
+	id VARCHAR(30) REFERENCES Phones(id),
+	bssid VARCHAR(17) REFERENCES Aps(bssid),
+	PRIMARY KEY (time, id, bssid)
+);
